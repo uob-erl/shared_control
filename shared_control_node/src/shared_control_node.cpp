8c9a6a24c487a3766fe35a486efa5621973ecc43
Pantelis Pappas <pantelpapp@gmail.com>
2020-02-25 15:33:33 +0000

    added comments

--- shared_control_node/src/shared_control_node.cpp
+++ shared_control_node/src/shared_control_node.cpp
@@ -1,3 +1,8 @@
+/*
+This node combines the controller's inputs with the outputs of the VFH+ alghorithm.
+While it still consists of a simple operation of addtion, it can and should be further modifies
+for a more optimized input blending. For simple testing though, it should work just fine.
+*/
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 
@@ -27,14 +32,16 @@ public:
 
 Shared_Control::Shared_Control()
 {
-  teleop_sub_ = nh_.subscribe("/teleop/cmd_vel", 5, &Shared_Control::teleCallback,this);
-  vfh_sub_ = nh_.subscribe("/vfh/cmd_vel", 5, &Shared_Control::vfhCallback, this);
+  teleop_sub_ = nh_.subscribe("/teleop/cmd_vel", 5, &Shared_Control::teleCallback,this); //subscriber to controller commands
+  vfh_sub_ = nh_.subscribe("/vfh/cmd_vel", 5, &Shared_Control::vfhCallback, this); //subscriber to VFH+ output
   shared_cmd_ = nh_.advertise<geometry_msgs::Twist>("/shared_control/cmd_vel", 5);
   timer = nh_.createTimer(ros::Duration(0.1), &Shared_Control::operationCallback, this);
 }
 
 void Shared_Control::teleCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
+//  Teleoperation callback. We store the values of the controler
+//  input to class variables.
   tele.linear.x = msg->linear.x;
   tele.linear.y = msg->linear.y;
   tele.linear.z = msg->linear.z;
@@ -44,6 +51,7 @@ void Shared_Control::teleCallback(const geometry_msgs::Twist::ConstPtr& msg)
   }
 void Shared_Control::vfhCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
+  //  Same as teleCallback.
   vfh.linear.x = msg->linear.x;
   vfh.linear.y = msg->linear.y;
   vfh.linear.z = msg->linear.z;
@@ -53,22 +61,47 @@ void Shared_Control::vfhCallback(const geometry_msgs::Twist::ConstPtr& msg)
 }
 
 void Shared_Control::operationCallback(const ros::TimerEvent&)
+/* We combine the controller and VFH+ inputs.
+ We care about the angural x input mostly and ignore the linear x at the moment
+This CallBack can be modified freely for more optimized input blending.*/
 {
+  if (tele.linear.x == 0 & tele.angular.z == 0){
+    /*
+     * This is just so VFH+ doesn't move the robot, if the operator is not touching the stick at all
+      */
+    cmd_vel.linear.x = 0;
+    cmd_vel.linear.y = 0;
+    cmd_vel.linear.z = 0;
+
+    cmd_vel.angular.x = 0;
+    cmd_vel.angular.y = 0;
+    cmd_vel.angular.z = 0;
+  }
+  else
+  {
+
   cmd_vel.linear.x = tele.linear.x /*+ vfh.linear.x*/;
   cmd_vel.linear.y = tele.linear.y + vfh.linear.y;
   cmd_vel.linear.z = tele.linear.z + vfh.linear.z;
 
   cmd_vel.angular.x = tele.angular.x + vfh.angular.x;
   cmd_vel.angular.y = tele.angular.y + vfh.angular.y;
-  cmd_vel.angular.z = tele.angular.z - vfh.angular.z; //
+  cmd_vel.angular.z = tele.angular.z - vfh.angular.z;
+
+  /*
+   * The reason there is a '-' here is because VFH+ outputs
+     negative values for the -->(right) direction
+     possitive values for the <--(left) direction
+     whereas teleop outputs
+     possitive values for the -->(right) direction
+     negative values for the <--(left) direction
+
+     this is honestly a quick fix and works fine at the moment
+    */
+  }
 
   shared_cmd_.publish(cmd_vel);
 }
-
-//void Shared_Control::cmd_velCallback(const geometry_msgs::Twist& msg){
-
-//}
-
 
 int main(int argc, char **argv)
 {
